79347804678d31340e5ca4689d5b9911113175ea
Salma Moubarki <salmamoubarki@gmail.com>
2024-05-02 22:26:56 +0200

    resolve Fiche de contact

---
+++ QuatreCoins/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import{ FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  hideAge:boolean = false;
+  something: string = '';
+}
